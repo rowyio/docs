f2845f0a8fa36e02c6385e8094fd81be968b0b67
Sidney Alcantara <sidney@sidney.me>
2021-09-27 11:45:59 +1000

    add video to create table

---
+++ src/components/Video.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+export default function Video(props) {
+  return (
+    <div style={{ position: "relative", paddingTop: "56.25%" }}>
+      <ReactPlayer
+        {...props}
+        style={{ position: "absolute", top: 0, left: 0 }}
+        width="100%"
+        height="100%"
+      />
+    </div>
+  );
+}
